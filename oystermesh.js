b761c4e10c51b7f65a63da1791b5b7e734845eb0
Bruno Block <bruno@oyster.org>
2020-03-04 11:01:58 +0000

    Sybil resistance upgrade

--- oystermesh.js
+++ oystermesh.js
@@ -29,7 +29,7 @@ const OY_BLOCK_COMMAND_QUOTA = 20000;
 const OY_BLOCK_RANGE_KILL = 0.7;
 const OY_BLOCK_RANGE_MIN = 5;//10, minimum syncs/dives required to not locally reset the meshblock, higher means side meshes die easier
 const OY_BLOCK_BOOT_BUFFER = 360;//seconds grace period to ignore certain cloning/peering rules to bootstrap the network during a boot-up event
-const OY_BLOCK_BOOT_SEED = 1583318700;//timestamp to boot the mesh, node remains offline before this timestamp
+const OY_BLOCK_BOOT_SEED = 1583319900;//timestamp to boot the mesh, node remains offline before this timestamp
 const OY_BLOCK_SECTORS = [[30, 30000], [50, 50000], [51, 51000], [52, 52000], [58, 58000], [60, 60000]];//timing definitions for the meshblock
 const OY_BLOCK_BUFFER_CLEAR = [0.5, 500];
 const OY_BLOCK_BUFFER_SPACE = [12, 12000];//lower value means full node is eventually more profitable (makes it harder for edge nodes to dive), higher means better connection stability/reliability for self
@@ -2287,7 +2287,7 @@ function oy_data_soak(oy_node_id, oy_data_raw) {
 
                if (oy_data[0]==="OY_BLOCK_SYNC") {
                    if (oy_peer_flag===false||oy_state_current()!==2) {
-                       oy_node_deny(oy_node_id, "OY_DENY_SYNC_MISALIGN");
+                       if (oy_state_current(true)!==2) oy_node_deny(oy_node_id, "OY_DENY_SYNC_MISALIGN");
                        return false;
                    }
 
