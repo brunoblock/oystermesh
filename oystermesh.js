428dfaa0f6cf333364d409f18ad9341b0e6b1575
Bruno Block <bruno@oyster.org>
2020-03-04 10:43:25 +0000

    Sybil resistance upgrade

--- oystermesh.js
+++ oystermesh.js
@@ -29,7 +29,7 @@ const OY_BLOCK_COMMAND_QUOTA = 20000;
 const OY_BLOCK_RANGE_KILL = 0.7;
 const OY_BLOCK_RANGE_MIN = 5;//10, minimum syncs/dives required to not locally reset the meshblock, higher means side meshes die easier
 const OY_BLOCK_BOOT_BUFFER = 360;//seconds grace period to ignore certain cloning/peering rules to bootstrap the network during a boot-up event
-const OY_BLOCK_BOOT_SEED = 1583318100;//timestamp to boot the mesh, node remains offline before this timestamp
+const OY_BLOCK_BOOT_SEED = 1583318700;//timestamp to boot the mesh, node remains offline before this timestamp
 const OY_BLOCK_SECTORS = [[30, 30000], [50, 50000], [51, 51000], [52, 52000], [58, 58000], [60, 60000]];//timing definitions for the meshblock
 const OY_BLOCK_BUFFER_CLEAR = [0.5, 500];
 const OY_BLOCK_BUFFER_SPACE = [12, 12000];//lower value means full node is eventually more profitable (makes it harder for edge nodes to dive), higher means better connection stability/reliability for self
@@ -491,9 +491,12 @@ function oy_worker(oy_worker_status) {
             else if (oy_work_type===1) {
                 let [oy_self_public, oy_block_next, oy_block_hash, oy_work_difficulty] = oy_work_data;
 
-                oy_work_thread = true;
-                let oy_work_solution = oy_work_mine(oy_hash_gen(oy_block_next+oy_self_public+oy_block_hash), oy_work_difficulty);
-                if (!!oy_work_solution) postMessage([oy_work_type, [oy_work_solution]]);
+                if (oy_work_thread===false) {
+                    oy_work_thread = true;
+                    let oy_work_solution = oy_work_mine(oy_hash_gen(oy_block_next+oy_self_public+oy_block_hash), oy_work_difficulty);
+                    oy_work_thread = false;
+                    if (!!oy_work_solution) postMessage([oy_work_type, [oy_work_solution]]);
+                }
             }
             else if (oy_work_type===2) {
                 let [oy_data_payload, oy_dive_ledger, oy_crypt_short, oy_sync_expand, oy_block_boot, oy_block_time, oy_block_hash, oy_work_difficulty, oy_payload_size] = oy_work_data;
