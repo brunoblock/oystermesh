98438483ae34417b003db560c52c83f8a8be112b
Bruno Block <bruno@oyster.org>
2020-03-09 16:42:07 +0000

    Peering system upgrade

--- oystermesh.js
+++ oystermesh.js
@@ -50,6 +50,7 @@ const OY_WORK_DILUTE = 3;
 const OY_WORK_TARGET = 55;//1440x7/1 week, value in minutes, lower is harsher work that kicks nodes off the mesh more frequently, higher discourages new node operators and hence less decentralization
 const OY_AKOYA_DECIMALS = 100000000;//zeros after the decimal point for akoya currency
 const OY_AKOYA_LIQUID = 10000000*OY_AKOYA_DECIMALS;//akoya liquidity restrictions to prevent integer overflow
+const OY_AKOYA_MAX_SUPPLY = 100000000*OY_AKOYA_DECIMALS;
 const OY_AKOYA_FEE = 0.000001*OY_AKOYA_DECIMALS;//akoya fee per block
 const OY_AKOYA_ISSUANCE = OY_AKOYA_DECIMALS;//akoya issuance to divers per block in aggregate
 const OY_DNS_AUCTION_DURATION = 259200;//seconds of auction duration since latest valid bid - 3 days worth
@@ -103,8 +104,8 @@ const OY_DNS_OWNER_MIN = (OY_AKOYA_FEE+OY_DNS_FEE)*OY_DNS_AUCTION_MIN+(OY_AKOYA_
 //const OY_META_OWNER_MIN
 
 // TEMPLATE VARS
-//[[0]:peership block_time, [1]:latch flag, [2]:base sent, [3]:latency avg, [4]:latency history, [5]:data beam, [6]:data beam history, [7]:data soak, [8]:data soak history, [9]:mesh core cut]
-const OY_PEER_TEMPLATE = [null, null, false, 0, [], 0, [], 0, [], 0];//TODO put command counter in meshblock
+//[[0]:peership block_time, [1]:latch flag, [2]:base sent, [3]:latency avg, [4]:latency history, [5]:data beam, [6]:data beam history, [7]:data soak, [8]:data soak history, [9]:mesh core cut, [10]:signal_offer_push, [11]:signal_offer_pull]
+const OY_PEER_TEMPLATE = [null, null, false, 0, [], 0, [], 0, [], 0, null, null];//TODO put command counter in meshblock
 //the current meshblock - [[0]:[[0]:oy_mesh_dynasty, [1]:oy_block_timestamp, [2]:oy_mesh_range, [3]:oy_work_difficulty, [4]:oy_genesis_timestamp, [5]:oy_epoch_macro_count, [6]:oy_epoch_micro_count, [7]:oy_grade_total, [8]:oy_grade_current, [9]:oy_grade_avg, [10]:oy_grade_keep, [11]:oy_range_roll, [12]:oy_range_keep, [13]:oy_work_roll, [14]:oy_work_keep],
 // [1]:oy_dive_sector, [2]:oy_snapshot_sector, [3]:oy_command_sector, [4]:oy_akoya_sector, [5]:oy_dns_sector, [6]:oy_auction_sector, [7]:oy_meta_sector]
 const OY_BLOCK_TEMPLATE = Object.freeze([[null, null, null, null, null, null, null, null, null, null, [], null, [], null, []], {}, [[], []], {}, {}, {}, {}, {}, {}, {}]);//TODO figure out channel sector
@@ -957,6 +958,18 @@ function oy_peer_process(oy_peer_id, oy_data_flag, oy_data_payload) {
             }
         }
         return true;
+    }
+    else if (oy_data_flag==="OY_PEER_EXCHANGE_A") {
+
+    }
+    else if (oy_data_flag==="OY_PEER_EXCHANGE_B") {
+
+    }
+    else if (oy_data_flag==="OY_PEER_EXCHANGE_C") {
+
+    }
+    else if (oy_data_flag==="OY_PEER_EXCHANGE_D") {
+
     }
     else if (oy_data_flag==="OY_PEER_BASE") {//source is sending entire block, in chunks, to self as latch
         /*
@@ -2846,6 +2859,19 @@ function oy_block_engine() {
 
         //oy_chrono(oy_node_boost, OY_MESH_BUFFER[1]);
         //if (Object.keys(OY_PEERS).length<OY_PEER_MAX) oy_chrono(oy_node_assign, Math.ceil(Math.random()*OY_BLOCK_SECTORS[0][1]));
+
+        let oy_peer_map = {};
+        for (let oy_peer_select in OY_PEERS) {
+            oy_peer_map[oy_hash_gen(oy_peer_select)] = true;
+        }
+
+        for (let oy_peer_select in OY_PEERS) {
+            if (OY_PEERS[oy_peer_select][1]===0||OY_PEERS[oy_peer_select][0]>=OY_BLOCK_TIME-OY_BLOCK_SECTORS[5][0]) continue;
+            OY_PEERS[oy_peer_select][10] = new SimplePeer({initiator: true, trickle: false});
+            OY_PEERS[oy_peer_select][10].on('signal', oy_signal_data => {
+                oy_data_beam(oy_peer_select, "OY_PEER_EXCHANGE_A", [oy_peer_map, JSON.stringify(oy_signal_data)]);
+            });
+        }
 
         for (let oy_report_count = 10000; oy_report_count<=OY_BLOCK_SECTORS[5][1]; oy_report_count += 10000) {
             oy_chrono(oy_peer_report, oy_report_count);
